dea68db6a7ae236d5b495495454883b3f3ed3277
ViniciosLugli <vinicioslugli@gmail.com>
2024-08-21 17:02:10 -0300

    feat(backend): :sparkles: Added health route for the api

--- src/backend/src/routes/__init__.py
+++ src/backend/src/routes/__init__.py
@@ -1,6 +1,8 @@
 from .data import data_router
+from .health_check import health_router
 from fastapi import FastAPI
 
 
 def setup(app: FastAPI):
     app.include_router(data_router)
+    app.include_router(health_router)
---
+++ src/backend/src/routes/health_check.py
@@ -0,0 +1,10 @@
+from services import DataService
+from fastapi import APIRouter, FastAPI, Request, Response
+
+
+health_router = APIRouter()
+
+
+@health_router.get("/health")
+def health_check(request: Request) -> dict:
+    return {"status": "ok"}
