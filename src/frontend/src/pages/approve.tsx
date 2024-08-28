53d6beb89960a3900f6c05df5a95e35c7d238fc5
Edustn <eduardo.santos@sou.inteli.edu.br>
2024-08-27 22:08:30 -0300

    feat: insere tela de reprovacao

--- src/frontend/src/pages/approve.tsx
+++ src/frontend/src/pages/approve.tsx
@@ -1,10 +1,10 @@
 export default function Approve() {
 	return (
 		<div className="flex flex-col justify-center items-center min-h-screen">
-			<div className="text-7xl font-bold">Não possui falhas</div>
 			<div className="my-8">
 				<img className="max-w-350 max-h-350" src="/image 7.png" alt="Imagem ilustrativa" />
 			</div>
+			<div className="text-7xl font-bold">Não possui falhas</div>
 			<div className="text-4xl">Identificador de falhas</div>
 		</div>
 	);
