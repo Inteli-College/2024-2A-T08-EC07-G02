b0d3b82d3fcf64ef5bfe711db1f7445386eaadf2
Edustn <eduardo.santos@sou.inteli.edu.br>
2024-10-07 15:33:49 -0300

    feat: insere codigo para a nuvem

--- src/backend/app/storage/datalake.py
+++ src/backend/app/storage/datalake.py
@@ -8,16 +8,40 @@ class Datalake:
         self.database_url = os.getenv("DATALAKE_URL")
         self.__duckdb = None
 
+        # Set up AWS S3 credentials
+        self.s3_key_id = os.getenv("AWS_ACCESS_KEY_ID")
+        self.s3_secret = os.getenv("AWS_SECRET_ACCESS_KEY")
+        self.s3_region = os.getenv("AWS_SESSION_TOKEN")
+
+        # Configure DuckDB to connect to S3
+        self.configure_s3()
+
     @property
     def duckdb(self) -> duckdb.DuckDBPyConnection:
         return self.__duckdb
 
     def __enter__(self):
         if self.__duckdb is None:
-            self.__duckdb = duckdb.connect(database=self.database_url)
+            self.__duckdb = duckdb.connect()
+            self.__duckdb.sql(f"""
+                SET s3_region='{self.s3_region}';
+                SET s3_access_key_id='{self.s3_key_id}';
+                SET s3_secret_access_key='{self.s3_secret}';
+            """)
         return self.duckdb
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.__duckdb:
             self.__duckdb.close()
             self.__duckdb = None
+
+
+if __name__ == "__main__":
+    import dotenv
+    dotenv.load_dotenv()   
+    table_name = 'testegd'
+    data = {'col_1': [3, 2, 1, 0], 'col_2': ['a', 'b', 'c', 'd']}
+    df = pd.DataFrame.from_dict(data)
+    with Datalake() as client:
+        client.execute(f"INSERT INTO {table_name} SELECT * FROM df", {"df": df})
+        print(f"Data inserted into table '{table_name}' successfully.")
